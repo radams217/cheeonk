8df955a289cef933b820e87b4cf054c485498ef3
Ryan Adams <radams217@gmail.com>
2011-08-04 21:46:43 -0400

    package change

--- src/main/java/com/ryannadams/cheeonk/client/handler/ChatEventHandler.java
+++
@@ -1,9 +0,0 @@
-package com.ryannadams.cheeonk.client.handler;
-
-import com.google.gwt.event.shared.EventHandler;
-import com.ryannadams.cheeonk.client.event.ChatCreatedEvent;
-
-public interface ChatEventHandler extends EventHandler
-{
-	void onChatCreated(ChatCreatedEvent event);
-}
--- src/main/java/com/ryannadams/cheeonk/shared/action/IKey.java
+++
@@ -1,9 +0,0 @@
-package com.ryannadams.cheeonk.shared.action;
-
-import com.ryannadams.cheeonk.shared.ConnectionKey;
-
-public interface IKey
-{
-	ConnectionKey getConnectionKey();
-
-}
