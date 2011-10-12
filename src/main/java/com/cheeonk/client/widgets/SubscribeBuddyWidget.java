7fc1e7ec5e3adaf48deb93c8198e48f78a8fe6f2
Ryan Adams <radams217@gmail.com>
2011-10-11 20:49:35 -0400

    Subscribe stuff

--- src/main/java/com/cheeonk/client/widgets/SubscribeBuddyWidget.java
+++ src/main/java/com/cheeonk/client/widgets/SubscribeBuddyWidget.java
@@ -5,8 +5,10 @@ import net.customware.gwt.dispatch.client.DispatchAsync;
 import net.customware.gwt.dispatch.client.standard.StandardDispatchAsync;
 
 import com.cheeonk.client.callback.ChangedPresence;
+import com.cheeonk.client.callback.UpdatedBuddy;
 import com.cheeonk.shared.ConnectionKey;
 import com.cheeonk.shared.action.ChangePresence;
+import com.cheeonk.shared.action.UpdateBuddy;
 import com.cheeonk.shared.buddy.CheeonkPresence;
 import com.cheeonk.shared.buddy.IBuddy;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -15,6 +17,7 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.PushButton;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class SubscribeBuddyWidget extends Composite
@@ -22,6 +25,7 @@ public class SubscribeBuddyWidget extends Composite
 	private final DispatchAsync dispatchAsync;
 	private final VerticalPanel panel;
 
+	private final TextBox buddyName;
 	private final PushButton accept;
 	private final PushButton deny;
 
@@ -30,7 +34,7 @@ public class SubscribeBuddyWidget extends Composite
 		this.panel = new VerticalPanel();
 
 		this.dispatchAsync = new StandardDispatchAsync(new DefaultExceptionHandler());
-
+		this.buddyName = new TextBox();
 		this.accept = new PushButton("accept", new ClickHandler()
 		{
 			@Override
@@ -45,6 +49,17 @@ public class SubscribeBuddyWidget extends Composite
 					public void got(CheeonkPresence cheeonkPresence)
 					{
 						panel.clear();
+					}
+				});
+
+				buddy.setName(buddyName.getText());
+				dispatchAsync.execute(new UpdateBuddy(ConnectionKey.get(), buddy), new UpdatedBuddy()
+				{
+					@Override
+					public void got()
+					{
+						// TODO Auto-generated method stub
+
 					}
 				});
 
@@ -72,6 +87,10 @@ public class SubscribeBuddyWidget extends Composite
 		});
 
 		panel.add(new HTML(buddy.getJabberId().getJabberId() + " would like to add you as a buddy."));
+		HorizontalPanel buddyNamePanel = new HorizontalPanel();
+		buddyNamePanel.add(new HTML("Enter Buddy Name:"));
+		buddyNamePanel.add(buddyName);
+		panel.add(buddyNamePanel);
 		HorizontalPanel buttonPanel = new HorizontalPanel();
 		buttonPanel.add(accept);
 		buttonPanel.add(deny);
